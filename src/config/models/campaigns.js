48bb8fe795f028ac096f0d9a9c160713271a3803
sharvan-sharma <sharvansharma518@gmail.com>
2020-06-03 23:09:35 +0530

    min-campaign-c-1

--- src/config/models/campaigns.js
+++ src/config/models/campaigns.js
@@ -4,7 +4,8 @@ const campaignSchema = mongoose.Schema({
     name:{type:String,required:true},
     description:{type:String},
     status:{type:String,required:true},
-    staff_id:{type:String,required:true}
+    staff_id:{type:String,required:true},
+    createdAt:{type:Date,default:Date.now}
 })
 
 const Campaign = mongoose.model('campaigns',campaignSchema)
