76da8a8eb1cc75394c989553f0ed95c146732a1f
sharvan-sharma <sharvansharma518@gmail.com>
2020-06-07 12:14:59 +0530

    profile image upload route updated

--- routes/middleware/staff_middlewares/changeStaffProfilePhoto.js
+++ routes/middleware/staff_middlewares/changeStaffProfilePhoto.js
@@ -4,56 +4,51 @@ const path = require('path')
 const Staff = require('../../../src/config/models').Staff
 
 function changeStaffProfilePhoto(req,res,next){
-
     if(!req.user || req.user.account_type !== 'staff'){
         res.json({status:401,type:'unauthorised'})
     }else{
         const busboy = new Busboy({headers:req.headers,limits:{files:1,fileSize:512000}})
 
-        let fullPath = ''
-        let alternatePath = ''
-
-
         busboy.on('file',(fieldname, file, filename, encoding, mimetype)=>{
+
             let typeArray = ['jpg','bmp','png']
             let ext = filename.split('.').pop()
-
-            fullPath = path.join(process.cwd(),'/public/images/staff',req.user._id+'.'+ext)
-            alternatePath = path.join(process.cwd(),'/public/images/staff',req.user._id+'-2.'+ext)
-
+            
             if(typeArray.includes(ext)){
-                fs.exists(fullPath,(exists)=>{
-                    let wstream = fs.createWriteStream((exists)?alternatePath:fullPath)
+                    
+                    let storagePath = path.join('/images/staff',req.user._id+Date.now()+'.'+ext)
+                    let fullPath = path.join(process.cwd(),'/public',storagePath)
+                    let wstream = fs.createWriteStream(fullPath)
 
                     file.pipe(wstream)
 
                     file.on('limit',()=>{
-                        fs.unlink((exists)?alternatePath:fullPath,()=>{
+                        fs.unlink(fullPath,()=>{
                             limit_reach = true
                             res.json({status:455,type:'excceds'})
                         })
                     })
 
                     file.on('end',()=>{
-                        if(exists){
-                            fs.unlink(fullPath,()=>{
-                                fs.rename(alternatePath,fullPath,()=>{
-                                    res.json({status:200,photo:req.user.photo})
-                                })
-                            })
-                        }else{
-                            Staff.findOneAndUpdate(
+                        Staff.findOneAndUpdate(
                                 {   _id:req.user._id},
-                                {'$set':{photo:'/images/staff/'+req.user._id+'.'+ext}},
+                                {'$set':{photo:storagePath}},
                                 {new:true,strict:false},
                                 (err,staff)=>{
                                 if(err){res.json({status:500})}
-                                else if(staff){res.json({status:200,photo:staff.photo})}
+                                else if(staff){
+                                    if(req.user.photo === null){
+                                        res.json({status:200,photo:staff.photo})
+                                    }else{
+                                        let epath = path.join(process.cwd(),'/public',req.user.photo)
+                                        fs.unlink(epath,()=>{
+                                             res.json({status:200,photo:staff.photo})
+                                        })
+                                    }
+                                }
                                 else{res.json({status:422,type:'user doesnot exist'})}
                             })
-                        }
                     })
-                })
             }else{
                 res.json({status:423,type:'invalid_type'})
             }
