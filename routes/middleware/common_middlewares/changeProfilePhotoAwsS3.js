390cbbf2f3e80f0901c557d528919714fca26d9d
sharvan-sharma <sharvansharma518@gmail.com>
2020-06-20 18:36:56 +0530

    logging error remove from change Profile photo using aws s3

--- routes/middleware/common_middlewares/changeProfilePhotoAwsS3.js
+++ routes/middleware/common_middlewares/changeProfilePhotoAwsS3.js
@@ -40,7 +40,7 @@ function changeProfilePhotoAwsS3(req,res,next){
                         if(!limitCrossed){
                             res.json({status:500,type:'s3'})
                         }
-                        s3logger.error(`${req.user.accout_type} ${req.user.email} try to upload ${fname} got error.`)
+                        s3logger.error(`${req.user.account_type} ${req.user.email} try to upload ${fname} got error.`)
                     }else{
                         if(limitCrossed){
                             const delparam = {
@@ -49,15 +49,15 @@ function changeProfilePhotoAwsS3(req,res,next){
                             };
                             s3.deleteObject(delparam,(err,data)=>{
                                  if (err){
-                                     s3logger.error(`${req.user.accout_type} ${req.user.email} error while deleting ${fname} due to file size limit exceeds.`)
+                                     s3logger.error(`${req.user.account_type} ${req.user.email} error while deleting ${fname} due to file size limit exceeds.`)
                                  }else{
-                                     s3logger.info(`${req.user.accout_type} ${req.user.email} successfully deleted ${fname} due to file size limit exceeds.`)
+                                     s3logger.info(`${req.user.account_type} ${req.user.email} successfully deleted ${fname} due to file size limit exceeds.`)
                                  }
                             })
                         }else{
                             res.json({status:200,photo:data.Location})
 
-                            s3logger.info(`${req.user.accout_type} ${req.user.email} successfully updated profile photo to ${fname} `)
+                            s3logger.info(`${req.user.account_type} ${req.user.email} successfully updated profile photo to ${fname} `)
                             
                             const Model = (req.user.account_type === 'admin')?Admin:Staff
                             Model.findOneAndUpdate({_id:req.user._id},
@@ -65,7 +65,7 @@ function changeProfilePhotoAwsS3(req,res,next){
                                 {new:true,strict:false},
                                 (err,user)=>{
                                     if(err){
-                                        winslogger.error(`${req.user.accout_type} ${req.user.email} error while updating photo url`)
+                                        winslogger.error(`${req.user.account_type} ${req.user.email} error while updating photo url`)
                                     }else if(user){
                                         if(req.user.photo !== null){
                                             
@@ -76,9 +76,9 @@ function changeProfilePhotoAwsS3(req,res,next){
 
                                             s3.deleteObject(delparam2,(err,data)=>{
                                                 if (err){
-                                                    s3logger.error(`${req.user.accout_type} ${req.user.email} error while deleting ${fname} after updating with new one`)
+                                                    s3logger.error(`${req.user.account_type} ${req.user.email} error while deleting ${fname} after updating with new one`)
                                                 }else{
-                                                    s3logger.info(`${req.user.accout_type} ${req.user.email} successfully deleted ${fname} after updating with new one`)
+                                                    s3logger.info(`${req.user.account_type} ${req.user.email} successfully deleted ${fname} after updating with new one`)
                                                 }
                                             })
                                         }
